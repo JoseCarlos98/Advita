1697381be3fa9b3fe5400a0d4bc30f6b03316db4
Jose Carlos <josecarlos231998@gmail.com>
2021-06-07 13:48:18 -0600

    Avances.

--- js/index.js
+++ js/index.js
@@ -37,60 +37,7 @@ function cambiarFondo(x, p, lado) {
             circulo1.classList.remove("circulitos2-seleccionado");
             circulo2.classList.remove("circulitos2-seleccionado");
         }
-    } else {
-            
-        if (p == 'h') {
-            
-            if (x == 1) {
-                fondo.style.backgroundImage = "url(img/slide1.jpg)";
-                circulo1.classList.add("circulitos2-seleccionado");
-                circulo2.classList.remove("circulitos2-seleccionado");
-                circulo3.classList.remove("circulitos2-seleccionado");
-                num = 0;
-                
-            }else if(x == 2 ){
-                num = 1;
-                fondo.style.backgroundImage = "url(img/slide2.jpg)";
-                circulo2.classList.add("circulitos2-seleccionado");
-                circulo1.classList.remove("circulitos2-seleccionado");
-                circulo3.classList.remove("circulitos2-seleccionado");
-            }else{
-                num = 2;
-                fondo.style.backgroundImage = "url(img/slide3.jpg)";
-                circulo3.classList.add("circulitos2-seleccionado");
-                circulo1.classList.remove("circulitos2-seleccionado");
-                circulo2.classList.remove("circulitos2-seleccionado");
-            }
-        }
-        else if (p == 'c'){
-            if (x == 1) {
-                fondo.style.backgroundImage = "url(img/slide1.jpg)";
-            }else if(x == 2 ){
-                fondo.style.backgroundImage = "url(img/slide2.jpg)";
-            }else{
-                fondo.style.backgroundImage = "url(img/slide3.jpg)";
-            }
-        }
-        else{
-            if (x == 1) {
-                fondo.style.backgroundImage = "url(img/slide1.jpg)";
-                circuloo1.classList.add("circulitos2-seleccionado");
-                circuloo2.classList.remove("circulitos2-seleccionado");
-                circuloo3.classList.remove("circulitos2-seleccionado");
-                
-            }else if(x == 2 ){
-                fondo.style.backgroundImage = "url(img/slide2.jpg)";
-                circuloo2.classList.add("circulitos2-seleccionado");
-                circuloo1.classList.remove("circulitos2-seleccionado");
-                circuloo3.classList.remove("circulitos2-seleccionado");
-            }else{
-                fondo.style.backgroundImage = "url(img/slide3.jpg)";
-                circuloo3.classList.add("circulitos2-seleccionado");
-                circuloo1.classList.remove("circulitos2-seleccionado");
-                circuloo2.classList.remove("circulitos2-seleccionado");
-            }
-        }
-    }
+    } 
 }
 
     // MENU FIJO
@@ -104,7 +51,7 @@ function cambiarFondo(x, p, lado) {
 
     window.addEventListener('scroll', function(){
     if((window.pageYOffset) > 35){
-        menu.classList.add('nav3');                                    
+        // menu.classList.add('nav3');                                    
         menu11.classList.add('cien');                                    
         aaa.classList.add('mg');                                    
         logo.classList.add('logo-fijo');      
@@ -125,46 +72,45 @@ function cambiarFondo(x, p, lado) {
     })   
 
 
-    document.querySelectorAll(".info-categorias img").forEach(el=>{
-        el.addEventListener("click", function(ev){
-            ev.stopPropagation();
-            this.parentNode.classList.add("active");
-        })
-    });
-    
-    document.querySelectorAll(".info-categorias").forEach(el=>{
-        el.addEventListener("click", function(ev){
-            this.classList.remove("active");
-        })
-    })
-
-    function mostrarModal(a,b,c,d,e){
-
-        let modal1 = document.getElementById(a);
-        let modal2 = document.getElementById(b);
-        let modal3 = document.getElementById(c);
-        let modal4 = document.getElementById(d);
-        let modal5 = document.getElementById(e);
+        const um = document.querySelector('.um');
+        const hc = document.querySelector('.hc');
+        const svg = document.querySelector('.seccion3 .container .cuadro-efecto .linea svg');
+        const textoum = document.querySelector('.seccion3 .container .cuadro-efecto .um p')
+        const textohc = document.querySelector('.seccion3 .container .cuadro-efecto .hc p')
+        const linea = document.querySelectorAll('.color')
+        const textum = document.querySelector('.seccion3 .container .textos h1')
+        const texthc = document.querySelector('.seccion3 .container .textos h2')
         
-        modal1.classList.add('ver');                                    
-        modal1.classList.add('modall');                                    
-        modal2.classList.add('info-modal');                                    
-        modal3.classList.add('modal-img');                                    
-        modal4.classList.add('modal-texto');                                    
-        modal5.classList.add('vericono');                                    
-    }
+        const hoverhc = document.querySelector('seccion3 .container .cuadro-efecto .hc:hover')
+        
+        
+        hc.addEventListener('click', ()=>{
+            um.classList.add('cambio');
+            hc.classList.add('cambio');
+            svg.classList.add('cambioColor');
+            textoum.style.background = 'none';
+            linea[0].style.background = '#0099DC';
+            linea[1].style.background = '#0099DC';
+            textohc.style.background = '#0099DC';
+            hc.style.background = 'none'
+            um.style.background = ''
+            textum.style.opacity = '0%'
+            texthc.style.opacity = '100%'
+        })
+        um.addEventListener('click', ()=>{
+            textum.style.opacity = '100%'
+            texthc.style.opacity = '0%'
+            um.style.background = 'none'
+            um.classList.remove('cambio');
+            hc.classList.remove('cambio');
+            hc.style.background = ''
+            svg.classList.remove('cambioColor');
+            textoum.style.background = '#E0007A';
+            textohc.style.background = 'none';
+            linea[0].style.background = '#E0007A';
+            linea[1].style.background = '#E0007A';
+        })
 
-    function cerrarModal(a,b,c,d,e){
-        let modal1 = document.getElementById(a);
-        let modal2 = document.getElementById(b);
-        let modal3 = document.getElementById(c);
-        let modal4 = document.getElementById(d);
-        let modal5 = document.getElementById(e);
 
-        modal1.classList.remove('ver');                                    
-        modal1.classList.remove('modall');                                    
-        modal2.classList.remove('info-modal');                                    
-        modal3.classList.remove('modal-img');                                    
-        modal4.classList.remove('modal-texto');                                    
-        modal5.classList.remove('vericono');
-    }
+
+
